9cf4a399082a23ef6b0a0767f3e0b776cd5365f8
ron bar yosef <ronibary@gmail.com>
2022-10-09 10:09:42 +0300

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package com.ron.dev;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello roni");
+		
+		
+		
+		
+
+	}
+
+}
---
+++ readme.txt
@@ -0,0 +1,1 @@
+test folder for git practice
